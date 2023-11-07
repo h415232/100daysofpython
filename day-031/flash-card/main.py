a26cf537923ac2bfd25cf950d57ed96d3b21ee60
h415232 <harveycruzado@gmail.com>
2023-11-07 22:09:59 +0800

    Day 31: Enabling reading from file and flashing new words randomly (2/4)

--- day-031/flash-card/main.py
+++ day-031/flash-card/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import pandas as pd
+import random as rand
 
 # GLOBAL VARIABLES
 BACKGROUND_COLOR = "#B1DDC6"
@@ -10,12 +12,12 @@ CARD_WIDTH = 800
 CARD_HEIGHT = 526
 CARD_FRONT_DIR = "images/card_front.png"
 
-TITLE_TXT = "Title"
+TITLE_TXT_PLACEHOLDER = "Title"
 TITLE_TXT_CONFIG = ("Ariel", 40, "italic")
 TITLE_TXT_XPOS = 400
 TITLE_TXT_YPOS = 150
 
-WORD_TXT = "Word"
+WORD_TXT_PLACEHOLDER = "Word"
 WORD_TXT_CONFIG = ("Ariel", 60, "bold")
 WORD_TXT_XPOS = 400
 WORD_TXT_YPOS = 263
@@ -24,6 +26,18 @@ TXT_COLOR_FILL = '#000000'
 
 CROSS_IMG_DIR = "images/wrong.png"
 CHECK_IMG_DIR = "images/right.png"
+
+DATA_DIR = "data/french_words.csv"
+
+# Get words from Dictionary
+def new_word():
+    word = rand.choice(word_dict)
+    canvas.itemconfig(title_txt, text="French")
+    canvas.itemconfig(word_txt, text=word["French"])
+
+# Load the French word
+df = pd.read_csv(DATA_DIR)
+word_dict = df.to_dict(orient="records")
 
 # Creating the window
 window = Tk()
@@ -38,31 +52,34 @@ card_front_img = PhotoImage(file=CARD_FRONT_DIR)
 canvas.create_image(CARD_WIDTH/2, CARD_HEIGHT/2, image=card_front_img)
 
 # Title Txt
-canvas.create_text(TITLE_TXT_XPOS, 
-                   TITLE_TXT_YPOS, 
-                   text=TITLE_TXT, 
-                   font=TITLE_TXT_CONFIG, 
-                   fill=TXT_COLOR_FILL)
+title_txt = canvas.create_text(TITLE_TXT_XPOS, 
+                               TITLE_TXT_YPOS, 
+                               text=TITLE_TXT_PLACEHOLDER, 
+                               font=TITLE_TXT_CONFIG, 
+                               fill=TXT_COLOR_FILL)
 
 # Word Txt
-canvas.create_text(WORD_TXT_XPOS, 
-                   WORD_TXT_YPOS, 
-                   text=WORD_TXT, 
-                   font=WORD_TXT_CONFIG, 
-                   fill=TXT_COLOR_FILL)
+word_txt = canvas.create_text(WORD_TXT_XPOS, 
+                              WORD_TXT_YPOS, 
+                              text=WORD_TXT_PLACEHOLDER, 
+                              font=WORD_TXT_CONFIG, 
+                              fill=TXT_COLOR_FILL)
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0,column=0, columnspan=2)
 
 # Unknown Button
 cross_image = PhotoImage(file=CROSS_IMG_DIR)
-unknown_btn = Button(image=cross_image, highlightthickness=0)
+unknown_btn = Button(image=cross_image, highlightthickness=0, command=new_word)
 unknown_btn.grid(row=1,column=0)
 
 # Known Button
 check_image = PhotoImage(file=CHECK_IMG_DIR)
-known_btn = Button(image=check_image, highlightthickness=0)
+known_btn = Button(image=check_image, highlightthickness=0, command=new_word)
 known_btn.grid(row=1,column=1)
+
+# Get initial word
+new_word()
 
 # Make the window persist until closed
 window.mainloop()
