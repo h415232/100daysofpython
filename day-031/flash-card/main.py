faf9de91922dbc3f77eeebc24af6e150dbd231a9
h415232 <harveycruzado@gmail.com>
2023-11-08 20:53:28 +0800

    Day 31: Added functionality to remember what has been learned and not learned (4/4)

--- day-031/flash-card/main.py
+++ day-031/flash-card/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pandas as pd
 import random as rand
+import os
 
 # CONSTANT VARIABLES
 BACKGROUND_COLOR = "#B1DDC6"
@@ -28,10 +29,14 @@ TXT_COLOR_FILL = '#000000'
 CROSS_IMG_DIR = "images/wrong.png"
 CHECK_IMG_DIR = "images/right.png"
 
-DATA_DIR = "data/french_words.csv"
+DATA_DFLT_DIR = "data/french_words.csv"
+DATA_TO_LEARN_DIR = "data/to_learn.csv"
+DATA_LEARNED_DIR = "data/learned.csv"
 
 # Global Variable
 word = {}
+known = []
+unknown = []
 
 # Get words from Dictionary
 def new_card():
@@ -40,7 +45,7 @@ def new_card():
     # Cancels the job for timer
     window.after_cancel(flip_timer)
 
-    word = rand.choice(word_dict)
+    word = rand.choice(unknown)
     canvas.itemconfig(card, image=card_front_img)
     canvas.itemconfig(title_txt, text="French", fill="black")
     canvas.itemconfig(word_txt, text=word["French"], fill="black")
@@ -48,14 +53,49 @@ def new_card():
     # Initiate the job for flip timer
     flip_timer = window.after(3000, func=flip_card)
 
+# Flip the card with the English translation of the Word
 def flip_card():
     canvas.itemconfig(card, image=card_back_img)
     canvas.itemconfig(title_txt, text="English", fill="white")
     canvas.itemconfig(word_txt, text=word["English"], fill="white")
 
+# If word is known, remove from the current list of words
+def known_cmd():
+    print("Log: Clicked Known BTN")
+    unknown.remove(word)
+    known.append(word)
+
+    new_card()
+
+# If word is unknown, add from the list of word to relearn
+def unknown_cmd():
+    print("Log: Clicked Unknown BTN")
+    new_card()
+
+def on_closing():
+    print("Log: Clicked the X button, closing the window")
+    to_learn_df = pd.DataFrame(unknown)
+    learned_df = pd.DataFrame(known)
+    
+    to_learn_df.to_csv(DATA_TO_LEARN_DIR, header=True, index=False)
+
+    if os.path.isfile(DATA_LEARNED_DIR):
+        learned_df.to_csv(DATA_LEARNED_DIR, header=False, index=False, mode='a')
+    else:
+        learned_df.to_csv(DATA_LEARNED_DIR, header=True, index=False)
+
+    window.destroy()
+
 # Load the French word
-df = pd.read_csv(DATA_DIR)
-word_dict = df.to_dict(orient="records")
+if os.path.isfile(DATA_TO_LEARN_DIR):
+    print("Log: File 'To Learn' exists!")
+    df = pd.read_csv(DATA_TO_LEARN_DIR)
+else:
+    print("Log: File 'To Learn' doesn't exits, reverting to default")
+    df = pd.read_csv(DATA_DFLT_DIR)
+
+# Generate a list of dictionary pairs [{'French':'frenchword', 'English':'englishword'},...]
+unknown = df.to_dict(orient="records")
 
 # Creating the window
 window = Tk()
@@ -93,12 +133,12 @@ canvas.grid(row=0,column=0, columnspan=2)
 
 # Unknown Button
 cross_image = PhotoImage(file=CROSS_IMG_DIR)
-unknown_btn = Button(image=cross_image, highlightthickness=0, command=new_card)
+unknown_btn = Button(image=cross_image, highlightthickness=0, command=unknown_cmd)
 unknown_btn.grid(row=1,column=0)
 
 # Known Button
 check_image = PhotoImage(file=CHECK_IMG_DIR)
-known_btn = Button(image=check_image, highlightthickness=0, command=new_card)
+known_btn = Button(image=check_image, highlightthickness=0, command=known_cmd)
 known_btn.grid(row=1,column=1)
 
 # Setup the flip timer
@@ -106,6 +146,9 @@ flip_timer = window.after(3000, func=flip_card)
 
 # Get initial word
 new_card()
+
+# Save the dataframe when window is closed
+window.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Make the window persist until closed
 window.mainloop()
